1913e7699bda9de9c6063ee9f54344cd596b2a00
Vid Luther <vid@luther.io>
2022-12-29 12:49:53 -0600

    removed a file we aren't using anynore

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-const webhooksRouter = require('./routes/webhooks')
+
 const app = express()
 
 app.use(logger('dev'))
