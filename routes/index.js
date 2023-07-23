f4a7dd965af1cc07a2f2a41f0caa8e1964b20ec4
Vid Luther <vid@luther.io>
2023-07-23 11:22:11 -0500

    added endpoint to capture checkouts

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const logger = require('morgan')
+const express = require("express");
+const router = express.Router();
+const logger = require("morgan");
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
 
-router.get('/webhooks', function (req, res, next) {
-  res.send('THIS IS A Webhook')
-})
+router.get("/webhooks", function (req, res, next) {
+  res.send("THIS IS A Webhook");
+});
 
 /* POST to Integromat. */
-router.post('/webhooks/integromat', function (req, res, next) {
-  console.log(req.body)
+router.post("/webhooks/departures", function (req, res, next) {
+  console.log(req.body);
   // make it look like it's taking a while
   // cuz integromat takes it's time sometimes
-  setTimeout(function() {
-    res.send('Accepted')
+  setTimeout(function () {
+    res.send("Accepted");
   }, 500);
+});
 
-})
+router.post("/webhooks/checkouts", function (req, res, next) {
+  console.log(req.body);
+  // make it look like it's taking a while
+  // cuz integromat takes it's time sometimes
+  setTimeout(function () {
+    res.send("Accepted");
+  }, 500);
+});
 
-function returnAccepted () {
-  return 'Accepted'
+function returnAccepted() {
+  return "Accepted";
 }
 
-module.exports = router
+module.exports = router;
